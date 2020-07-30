656902534ff60c615b778dd6df5e750b1339d01b
Ganesh Padhayay <ganeshraj020794@gmail.com>
2020-07-30 11:23:59 +0530

    hilt test setup added

---
+++ app/src/androidTest/java/com/example/android_hilt_sample/MainTest.kt
@@ -0,0 +1,25 @@
+package com.example.android_hilt_sample
+
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+@HiltAndroidTest
+class MainTest {
+
+    //we can use order available after JUnit 4.13 to give order to executions
+    @get:Rule(order = 0)
+    var hiltRule = HiltAndroidRule(this)
+
+    @Before
+    fun init() {
+        hiltRule.inject()
+    }
+
+    @Test
+    fun testSomething() {
+
+    }
+}
---
+++ app/src/androidTest/java/com/example/android_hilt_sample/MyTestRunner.kt
@@ -0,0 +1,17 @@
+package com.example.android_hilt_sample
+
+import android.app.Application
+import android.content.Context
+import androidx.test.runner.AndroidJUnitRunner
+import dagger.hilt.android.testing.HiltTestApplication
+
+class MyTestRunner : AndroidJUnitRunner() {
+
+    override fun newApplication(
+        cl: ClassLoader?,
+        className: String?,
+        context: Context?
+    ): Application {
+        return super.newApplication(cl, HiltTestApplication::class.java.name, context)
+    }
+}
